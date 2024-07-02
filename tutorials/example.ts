cded05c045c081574b3cd578e98364cb6a21bda5
Shogo Wada <aiz4338150@gmail.com>
2024-07-02 14:12:24 +0900

    first step

---
+++ tutorials/example.ts
@@ -0,0 +1,19 @@
+type User = {
+  name: string;
+  age: number;
+};
+
+function isAdult(user: User): boolean {
+  return user.age >= 18;
+}
+
+const justine: User = {
+  name: 'Justine',
+  age: 23,
+};
+
+const isJustineAnAdult: boolean = isAdult(justine);
+
+console.log(isJustineAnAdult); // true
+
+console.log(WebAssembly);
